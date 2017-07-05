a658d21d50d49f5f5e3dda988a51c7dd591f97e6
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-07-05 20:02:41 +0530

    included node and container metrics

--- prometheus/promUp.sh
+++ prometheus/promUp.sh
@@ -2,6 +2,8 @@
 
 ################################################################################################
 #                       Monitoring Solution with Prometheus Stack
+# 										NodeExporter
+# 										cAdvisor
 #                                       Prometheus
 #                                       Grafana
 ################################################################################################
@@ -21,6 +23,15 @@ function wait_for_service {
 
 network="prom-net"
 LABEL_GROUP="prom-monitoring"
+
+NODE_EXPORTER_IMG_NAME="prom/node-exporter"
+NODE_EXPORTER_IMG_TAG="latest"
+NODE_EXPORTER_SERVICE="node"
+
+CONTAINER_EXPORTER_IMG_NAME="google/cadvisor"
+CONTAINER_EXPORTER_IMG_TAG="latest"
+CONTAINER_EXPORTER_SERVICE="cadvisor"
+
 PROMETHEUS_IMG_NAME="ekambaram/prom"
 PROMETHEUS_IMG_TAG="1.7.1"
 PROMETHEUS_SERVICE_NAME="prometheus"
@@ -44,6 +55,51 @@ else
   sleep 4s
 fi
 
+################################################################################################
+#Create node-exporter
+################################################################################################
+# Spin up official NodeExporter Docker image
+docker service ls --filter label=com.group=$LABEL_GROUP |grep $NODE_EXPORTER_SERVICE
+serviceFound=$?
+if [ $serviceFound != 0 ]; then
+  echo "Creating the $NODE_EXPORTER_SERVICE service..."
+  docker service create \
+    --name $NODE_EXPORTER_SERVICE \
+    --mode global \
+    --network $network \
+    --label com.group="$LABEL_GROUP" \
+    --mount type=bind,source=/proc,target=/host/proc \
+    --mount type=bind,source=/sys,target=/host/sys \
+    --mount type=bind,source=/,target=/rootfs \
+    $NODE_EXPORTER_IMG_NAME:$NODE_EXPORTER_IMG_TAG \
+    -collector.procfs /host/proc \
+    -collector.sysfs /host/proc \
+    -collector.filesystem.ignored-mount-points "^/(sys|proc|dev|host|etc)($|/)"
+else
+  echo "[ SERVICE IS ALREADY RUNNING ] $NODE_EXPORTER_SERVICE"
+fi
+
+################################################################################################
+# cadvisor
+################################################################################################
+# Spin up official cadvisor Docker image
+docker service ls --filter label=com.group=$LABEL_GROUP |grep $CONTAINER_EXPORTER_SERVICE
+serviceFound=$?
+if [ $serviceFound != 0 ]; then
+  echo "Creating the $CONTAINER_EXPORTER_SERVICE service..."
+  docker service create \
+    --name $CONTAINER_EXPORTER_SERVICE \
+    --mode global \
+    --network $network \
+    --label com.group="$LABEL_GROUP" \
+    --mount type=bind,source=/,target=/rootfs \
+    --mount type=bind,source=/var/run,target=/var/run \
+    --mount type=bind,source=/sys,target=/sys \
+    --mount type=bind,source=/var/lib/docker/,target=/var/lib/docker \
+    $CONTAINER_EXPORTER_IMG_NAME:$CONTAINER_EXPORTER_IMG_TAG
+else
+  echo "[ SERVICE IS ALREADY RUNNING ] $CONTAINER_EXPORTER_SERVICE"
+fi
 
 ################################################################################################
 #Create Prometheus
@@ -57,6 +113,7 @@ if [ $serviceFound != 0 ]; then
     --network $network \
     --publish $PROMETHEUS_SERVICE_PORT:$PROMETHEUS_SERVICE_PORT \
     --label com.group="$LABEL_GROUP" \
+    --mount "type=volume,source=promdata,volume-driver=azurefile,target=/prometheus" \
     --constraint 'node.role == manager' \
     $PROMETHEUS_IMG_NAME:$PROMETHEUS_IMG_TAG
 else
@@ -78,7 +135,7 @@ if [ $serviceFound != 0 ]; then
   --publish $GRAFANA_SERVICE_PORT:$GRAFANA_SERVICE_PORT \
   -e "GF_SECURITY_ADMIN_PASSWORD=$GRAFANA_ADMIN_PASSWORD" \
   -e "GF_USERS_ALLOW_SIGN_UP=false" \
-  -e "PROMETHEUS_ENDPOINT=http://prometheus:9090" \
+  -e "PROMETHEUS_ENDPOINT=prometheus:9090" \
   -e "IMPORT_DASHBOARDS=Y" \
   --label com.group="$LABEL_GROUP" \
   --constraint 'node.role == manager' \
