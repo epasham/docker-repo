1426a5b9c9aee45bc6470d1721df13dd887294a5
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-07-12 13:29:47 +0530

    Update managementUp.sh

--- management/managementUp.sh
+++ management/managementUp.sh
@@ -8,7 +8,7 @@ docker service create \
 docker service create \
   --publish=8000:8080 \
   --limit-cpu 0.5 \
-  --name=viz \
+  --name visualizer \
   --constraint=node.role==manager \
   --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
   dockersamples/visualizer
