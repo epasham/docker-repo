488dc0d4c9c7180e5ae81e24817213f1b428b378
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-07-12 13:25:00 +0530

    Create managementUp.sh

---
+++ management/managementUp.sh
@@ -0,0 +1,14 @@
+docker service create \
+  --publish 9000:9000 \
+  --name portainer \
+  --constraint=node.role==manager \
+  --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+  portainer/portainer 
+
+docker service create \
+  --publish=8000:8080 \
+  --limit-cpu 0.5 \
+  --name=viz \
+  --constraint=node.role==manager \
+  --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
+  dockersamples/visualizer
