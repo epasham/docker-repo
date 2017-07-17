725d2ffb952fc456f5e00448637ca86870bd0db0
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-07-17 19:58:34 +0530

    Update loggingUp.sh

--- logging/elk-logspout/loggingUp.sh
+++ logging/elk-logspout/loggingUp.sh
@@ -11,7 +11,7 @@
 function wait_for_service {
     SERVICE_NAME=$1
     while true; do
-        REPLICAS=$(docker service ls --filter "name=${SERVICE_NAME}" | awk '{print $4}')
+        REPLICAS=$(docker service ls --filter "name=${SERVICE_NAME}" |awk 'NR>1'| awk '{print $3}')
         if [[ ${REPLICAS} == "1/1" ]]; then
             break
         else
@@ -25,20 +25,20 @@ network="logging-net"
 LABEL_GROUP="ellk-logging"
 
 ES_IMG_NAME="docker.elastic.co/elasticsearch/elasticsearch"
-ES_IMG_TAG="5.3.2"
+ES_IMG_TAG="5.4.2"
 ES_SERVICE_NAME="elasticsearch"
 ES_SERVICE_PORT="9200"
 
 LOGSTASH_IMG_NAME="docker.elastic.co/logstash/logstash"
-LOGSTASH_IMG_TAG="5.3.2"
-LOGSTASH_SERVICE_NAME="kibana"
+LOGSTASH_IMG_TAG="5.4.2"
+LOGSTASH_SERVICE_NAME="logstash"
 
 LOGSPOUT_IMG_NAME="ekambaram/logspout-logstash"
 LOGSPOUT_IMG_TAG="v1"
 LOGSPOUT_SERVICE_NAME="logspout"
 
-KIBANA_IMG_NAME="docker.elastic.co/kibana/kibana"
-KIBANA_IMG_TAG="5.3.2"
+KIBANA_IMG_NAME="kibana"
+KIBANA_IMG_TAG="5.4.2"
 KIBANA_SERVICE_NAME="kibana"
 KIBANA_SERVICE_PORT="5601"
 
@@ -67,12 +67,12 @@ if [ $serviceFound != 0 ]; then
     --network $network \
     --label com.group="$LABEL_GROUP" \
     --constraint 'node.role == manager' \
-	-e "ES_JAVA_OPTS=-Xms256m -Xmx256m" \
+    -e "LOGSPOUT=ignore" \
+    -e "ES_JAVA_OPTS=-Xms256m -Xmx256m" \
     -e "xpack.security.enabled=false" \
     -e "xpack.monitoring.enabled=false" \
     -e "xpack.graph.enabled=false" \
     -e "xpack.watcher.enabled=false" \
-    -e "LOGSPOUT=ignore" \
     --mount type=volume,source=esdata,target=/usr/share/elasticsearch/data \
     $ES_IMG_NAME:$ES_IMG_TAG
 else
@@ -93,7 +93,7 @@ if [ $serviceFound != 0 ]; then
     --name $LOGSTASH_SERVICE_NAME \
     --network $network \
     --label com.group="$LABEL_GROUP" \
-    --mount type=volume,source=/logstash/logstash.conf,target=/usr/share/logstash/pipeline/logstash.conf \
+    --mount type=bind,source=/logstash/logstash.conf,target=/usr/share/logstash/pipeline/logstash.conf \
     $LOGSTASH_IMG_NAME:$LOGSTASH_IMG_TAG
 else
   echo "[ SERVICE IS ALREADY RUNNING ] $LOGSTASH_SERVICE_NAME"
@@ -107,13 +107,14 @@ wait_for_service $LOGSTASH_SERVICE_NAME
 docker service ls --filter label=com.group=$LABEL_GROUP |grep $LOGSPOUT_SERVICE_NAME
 serviceFound=$?
 if [ $serviceFound != 0 ]; then
-  echo "Creating the $LOGSTASH_SERVICE_NAME service..."
+  echo "Creating the $LOGSPOUT_SERVICE_NAME service..."
   docker service create \
     --name $LOGSPOUT_SERVICE_NAME \
     --network $network \
-	--mode global \
+    --mode global \
+    -e "ROUTE_URIS=logstash://logstash:5000" \
     --label com.group="$LABEL_GROUP" \
-	--mount "type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock" \
+    --mount "type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock" \
     $LOGSPOUT_IMG_NAME:$LOGSPOUT_IMG_TAG
 else
   echo "[ SERVICE IS ALREADY RUNNING ] $LOGSPOUT_SERVICE_NAME"
