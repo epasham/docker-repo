ea11a1be21f32130aafe05986231cc630805f102
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-08-21 20:02:08 +0530

    Create jenkinsDown.sh

---
+++ ci-cd-ct/jenkins-master-slave/jenkinsDown.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+services=(jenkins worker)
+networks=(ci-net)
+
+for service in "${services[@]}"
+do
+  serviceFound=$(docker service ls | awk '{print $2}'|grep $service|wc -l)
+#  echo "Return Value: $serviceFound"
+  if [ $serviceFound -eq 1 ]; then 
+    echo "[SHUTTING DOWN] $service" 
+    docker service rm $service
+  else
+    echo "[ SERVICE NOT FOUND ] $service"
+  fi
+done
+
+
+for network in "${networks[@]}"
+do 
+  networkFound=$(docker network ls | awk '{print $2}' |grep $network|wc -l)
+  if [ $networkFound -eq 1 ]; then
+    echo "[DELETING NETWORK] $network" 
+    docker network rm $network
+  else
+    echo "[ NETWORK NOT FOUND ] $network"
+  fi
+done
+
+
+
