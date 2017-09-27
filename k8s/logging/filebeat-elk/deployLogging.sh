2d1c696f830406c99cdbc190ff4f0dd1c4d5f2bf
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-09-27 19:20:50 +0530

    Create deployLogging.sh

---
+++ k8s/logging/filebeat-elk/deployLogging.sh
@@ -0,0 +1,6 @@
+kubectl create -f k8s-ns-logging.yml
+kubectl create -f k8s-logging-cfg.yml
+kubectl create -f k8s-logging-elasticsearch.yml
+kubectl create -f k8s-logging-kibana.yml
+kubectl create -f k8s-logging-logstash.yml
+kubectl create -f k8s-logging-filebeat.yml
