16573d640753bffaf823deba0ebc70ceb17eecb4
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-09-29 15:37:56 +0530

    Update deployLogging.sh

--- k8s/logging/filebeat-elk/deployLogging.sh
+++ k8s/logging/filebeat-elk/deployLogging.sh
@@ -1,4 +1,4 @@
-kubectl create -f k8s-ns-logging.yml
+#kubectl create -f k8s-ns-logging.yml
 kubectl create -f k8s-logging-cfg.yml
 kubectl create -f k8s-logging-elasticsearch.yml
 kubectl create -f k8s-logging-kibana.yml
