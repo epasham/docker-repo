9ae42988d37a31ca5951a949117f503fc080fa24
epasham <epasham@cisco.com>
2017-10-06 09:35:43 +0000

    added mon scripts

---
+++ k8s/monitoring/deployMonitoring.sh
@@ -0,0 +1,5 @@
+kubectl create -f k8s-monitoring-cfg.yml
+kubectl create -f k8s-monitoring-elasticsearch.yml
+kubectl create -f k8s-monitoring-kibana.yml
+kubectl create -f k8s-monitoring-logstash.yml
+kubectl create -f k8s-monitoring-filebeat.yml
---
+++ k8s/monitoring/undeployMonitoring.sh
@@ -0,0 +1,5 @@
+kubectl delete -f k8s-monitoring-cfg.yml
+kubectl delete -f k8s-monitoring-elasticsearch.yml
+kubectl delete -f k8s-monitoring-kibana.yml
+kubectl delete -f k8s-monitoring-logstash.yml
+kubectl delete -f k8s-monitoring-filebeat.yml
