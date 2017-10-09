1cea585656af770289e73fe2b2dd2b567888b327
epasham <epasham@cisco.com>
2017-10-09 07:25:53 +0000

    done

--- k8s/monitoring/deployMonitoring.sh
+++ k8s/monitoring/deployMonitoring.sh
@@ -1,5 +1,2 @@
+kubectl create -f k8s-monitoring-ns.yml
 kubectl create -f k8s-monitoring-cfg.yml
-kubectl create -f k8s-monitoring-elasticsearch.yml
-kubectl create -f k8s-monitoring-kibana.yml
-kubectl create -f k8s-monitoring-logstash.yml
-kubectl create -f k8s-monitoring-filebeat.yml
--- k8s/monitoring/undeployMonitoring.sh
+++ k8s/monitoring/undeployMonitoring.sh
@@ -1,5 +1,1 @@
 kubectl delete -f k8s-monitoring-cfg.yml
-kubectl delete -f k8s-monitoring-elasticsearch.yml
-kubectl delete -f k8s-monitoring-kibana.yml
-kubectl delete -f k8s-monitoring-logstash.yml
-kubectl delete -f k8s-monitoring-filebeat.yml
