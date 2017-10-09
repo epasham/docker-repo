aded89bf329d9029e9d3eb394ddb7552a012fe92
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-10-09 05:53:25 -0400

    Update deployMonitoring.sh

--- k8s/monitoring/deployMonitoring.sh
+++ k8s/monitoring/deployMonitoring.sh
@@ -1,4 +1,5 @@
 #kubectl create -f k8s-monitoring-ns.yml
 kubectl create -f k8s-monitoring-cfg.yml
+kubectl create -f k8s-monitoring-prom-rules-cfg.yml
 kubectl create -f k8s-monitoring-kube-state-metrics.yml
 kubectl create -f k8s-monitoring-node-exporter.yml
