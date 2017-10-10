9479de5ac5eb45ffb4ce91a03631fa9a316cd322
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-10-10 12:41:25 +0530

    Update deployMonitoring.sh

--- k8s/monitoring/deployMonitoring.sh
+++ k8s/monitoring/deployMonitoring.sh
@@ -4,3 +4,4 @@ kubectl create -f k8s-monitoring-prom-rules-cfg.yml
 kubectl create -f k8s-monitoring-kube-state-metrics.yml
 kubectl create -f k8s-monitoring-node-exporter.yml
 kubectl create -f k8s-monitoring-prometheus.yml
+kubectl create -f k8s-monitoring-grafana.yml
