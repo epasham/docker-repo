dce5e5e903c6c246246fba9db45cb0067675c6d9
Ekambaram Pasham <epasham@users.noreply.github.com>
2017-10-12 08:55:54 -0400

    Update undeployMonitoring.sh

--- k8s/monitoring/undeployMonitoring.sh
+++ k8s/monitoring/undeployMonitoring.sh
@@ -1,6 +1,7 @@
 kubectl delete -f k8s-monitoring-grafana.yml
 kubectl delete -f k8s-monitoring-kube-state-metrics.yml
 kubectl delete -f k8s-monitoring-node-exporter.yml
+kubectl delete -f k8s-monitoring-cadvisor.yml
 kubectl delete -f k8s-monitoring-prometheus.yml
 kubectl delete -f k8s-monitoring-prom-rules-cfg.yml
 kubectl delete -f k8s-monitoring-cfg.yml
