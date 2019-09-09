8c5008fb9d1604ea0e0347f8d2baaa83559526e4
Ekambaram Pasham <epasham@users.noreply.github.com>
2019-09-09 15:36:15 +0530

    Update deployKubeProm.sh

--- k8s/monitoring/kube-prom/deployKubeProm.sh
+++ k8s/monitoring/kube-prom/deployKubeProm.sh
@@ -1,4 +1,22 @@
+#!/bin/sh
+
+# Verify/Create monitoring namespace
+kubectl get ns monitoring >/dev/null 2>&1
+if [ $? != 0 ]; then
+  echo "[ INFO ] Namespace: monitoring is not found. Creating"
+  kubectl create ns monitoring
+else
+  echo "[ INFO ] Namespace: monitoring is present"
+fi
+
+# Deploy CustomResourceDefinitions
 kubectl apply -f https://raw.githubusercontent.com/epasham/docker-repo/master/k8s/monitoring/kube-prom/01.crds.yaml
+until timeout 60s kubectl get crds/prometheuses.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done
+until timeout 60s kubectl get crds/alertmanagers.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done
+until timeout 60s kubectl get crds/podmonitors.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done
+until timeout 60s kubectl get crds/prometheusrules.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done
+until timeout 60s kubectl get crds/servicemonitors.monitoring.coreos.com; do echo "Waiting for CRDs to be created..."; done 
+
 kubectl apply -f https://raw.githubusercontent.com/epasham/docker-repo/master/k8s/monitoring/kube-prom/02.prom-operator.yaml
 kubectl apply -f https://raw.githubusercontent.com/epasham/docker-repo/master/k8s/monitoring/kube-prom/03.kube-state-metrics.yaml
 kubectl apply -f https://raw.githubusercontent.com/epasham/docker-repo/master/k8s/monitoring/kube-prom/04.node-exporter.yaml
