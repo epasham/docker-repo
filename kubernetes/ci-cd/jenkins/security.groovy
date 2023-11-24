8035e91f7a2633f15ee42193e7cee7922549bebe
Ekambaram Pasham <epasham@users.noreply.github.com>
2023-11-24 19:02:47 +0530

    Create security.groovy

---
+++ kubernetes/ci-cd/jenkins/security.groovy
@@ -0,0 +1,19 @@
+#!groovy
+
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+println "--> creating local user 'admin'"
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount(
+        System.getenv('JENKINS_ADMIN_USERNAME'),
+        System.getenv('JENKINS_ADMIN_PASSWORD'))
+
+instance.setSecurityRealm(hudsonRealm)
+
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
